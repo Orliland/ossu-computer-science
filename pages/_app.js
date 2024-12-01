39bdf9dc0f78af32862f1f4a2035125f2c8edf0f
Orlando Verdin <git@orli.land>
2024-12-01 08:56:56 -0700

    install simple analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,14 @@ import "../styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <script
+        async
+        src="https://scripts.simpleanalyticscdn.com/latest.js"
+      ></script>
+    </>
+  );
 }
